a2533c3721ed945f0c7a4e8970a7b9efdbcba0e0
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2018-07-05 07:18:48 -0400

    Update CommonProxy.java

--- src/main/java/net/TelepathicGrunt/UltraAmplified/CommonProxy.java
+++ src/main/java/net/TelepathicGrunt/UltraAmplified/CommonProxy.java
@@ -8,7 +8,6 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class CommonProxy {
 
 	public void preInit(FMLPreInitializationEvent event) {
-		 BiomeInit.registerBiomes();
 	}
 	
 	public void init(FMLInitializationEvent event) {
