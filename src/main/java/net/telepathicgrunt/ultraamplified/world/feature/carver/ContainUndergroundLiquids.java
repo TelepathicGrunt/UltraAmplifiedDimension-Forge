ede2809a1d95dcb24dadeccb56ec00da46900e5e
alex sarnese <alexthepokemon@Gmail.com>
2019-11-09 18:30:50 -0500

    added contains underground liquid to missing biomes

--- src/main/java/net/telepathicgrunt/ultraamplified/world/feature/carver/ContainUndergroundLiquids.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/world/feature/carver/ContainUndergroundLiquids.java
@@ -9,7 +9,6 @@ import com.mojang.datafixers.Dynamic;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.material.Material;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
