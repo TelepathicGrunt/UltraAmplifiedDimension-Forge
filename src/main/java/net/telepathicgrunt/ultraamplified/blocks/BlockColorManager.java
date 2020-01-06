0819d7f41d9ebddc0c6e25bd8e8fdc47cb3c97de
alex sarnese <alexthepokemon@Gmail.com>
2020-01-06 08:17:42 -0500

    Removed onlyin and used better coding practice

--- src/main/java/net/telepathicgrunt/ultraamplified/blocks/BlockColorManager.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/blocks/BlockColorManager.java
@@ -9,7 +9,6 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.world.GrassColors;
 import net.minecraft.world.biome.BiomeColors;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,44 +16,46 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.telepathicgrunt.ultraamplified.UltraAmplified;
 
 
-@Mod.EventBusSubscriber(modid = UltraAmplified.MODID, bus = Bus.MOD)
+@Mod.EventBusSubscriber(modid = UltraAmplified.MODID, bus = Bus.MOD, value = { Dist.CLIENT })
 public class BlockColorManager
 {
 
-	/**
-	 * Register the {@link IBlockColor} handlers.
-	 */
-	@SubscribeEvent
-	@OnlyIn(Dist.CLIENT)
-	public static void onBlockColorsInit(ColorHandlerEvent.Block event)
+	@Mod.EventBusSubscriber(modid = UltraAmplified.MODID, bus = Bus.MOD, value = { Dist.CLIENT })
+	private static class ForgeEvents
 	{
-		final BlockColors blockColors = event.getBlockColors();
-
-		//registers the colors for blocks that changes colors based on biome
-		blockColors.register((p_210225_0_, p_210225_1_, p_210225_2_, p_210225_3_) ->
+		/**
+		 * Register the {@link IBlockColor} handlers.
+		 */
+		@SubscribeEvent
+		public static void onBlockColorsInit(ColorHandlerEvent.Block event)
 		{
-			return p_210225_1_ != null && p_210225_2_ != null ? BiomeColors.func_228358_a_(p_210225_1_, p_210225_2_) : GrassColors.get(0.5D, 1.0D);
-		}, BlocksInit.GLOWGRASS_BLOCK.get());
-	}
-
-
-	/**
-	 * Register the {@link IItemColor} handlers
-	 */
-	@SubscribeEvent
-	@OnlyIn(Dist.CLIENT)
-	public static void onItemColorsInit(ColorHandlerEvent.Item event)
-	{
-		final BlockColors blockColors = event.getBlockColors();
-		final ItemColors itemColors = event.getItemColors();
-
-		// Use the Block's colour handler for an ItemBlock
-		final IItemColor itemBlockColourHandler = (stack, tintIndex) ->
+			final BlockColors blockColors = event.getBlockColors();
+	
+			//registers the colors for blocks that changes colors based on biome
+			blockColors.register((p_210225_0_, p_210225_1_, p_210225_2_, p_210225_3_) ->
+			{
+				return p_210225_1_ != null && p_210225_2_ != null ? BiomeColors.func_228358_a_(p_210225_1_, p_210225_2_) : GrassColors.get(0.5D, 1.0D);
+			}, BlocksInit.GLOWGRASS_BLOCK.get());
+		}
+	
+	
+		/**
+		 * Register the {@link IItemColor} handlers
+		 */
+		@SubscribeEvent
+		public static void onItemColorsInit(ColorHandlerEvent.Item event)
 		{
-			final BlockState state = ((BlockItem) stack.getItem()).getBlock().getDefaultState();
-			return blockColors.func_228054_a_(state, null, null, tintIndex);
-		};
-
-		itemColors.register(itemBlockColourHandler, BlocksInit.GLOWGRASS_BLOCK.get());
+			final BlockColors blockColors = event.getBlockColors();
+			final ItemColors itemColors = event.getItemColors();
+	
+			// Use the Block's colour handler for an ItemBlock
+			final IItemColor itemBlockColourHandler = (stack, tintIndex) ->
+			{
+				final BlockState state = ((BlockItem) stack.getItem()).getBlock().getDefaultState();
+				return blockColors.func_228054_a_(state, null, null, tintIndex);
+			};
+	
+			itemColors.register(itemBlockColourHandler, BlocksInit.GLOWGRASS_BLOCK.get());
+		}
 	}
 }
