f6acf7682dbe93108e82322e9809ddcb85c55233
alex sarnese <alexthepokemon@Gmail.com>
2020-01-10 18:33:46 -0500

    redid ugly code

--- src/main/java/net/telepathicgrunt/ultraamplified/world/feature/BoulderGiant.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/world/feature/BoulderGiant.java
@@ -6,7 +6,9 @@ import java.util.function.Function;
 import com.mojang.datafixers.Dynamic;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -15,105 +17,107 @@ import net.minecraft.world.gen.feature.BlockBlobConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.telepathicgrunt.ultraamplified.config.ConfigUA;
 
-public class BoulderGiant extends Feature<BlockBlobConfig> 
+
+public class BoulderGiant extends Feature<BlockBlobConfig>
 {
-    public BoulderGiant(Function<Dynamic<?>, ? extends BlockBlobConfig> configFactoryIn) {
+	public BoulderGiant(Function<Dynamic<?>, ? extends BlockBlobConfig> configFactoryIn)
+	{
 		super(configFactoryIn);
 	}
 
-
 	private final static Block mossyCobblestone = Blocks.MOSSY_COBBLESTONE;
-    private final static Block cobblestone = Blocks.COBBLESTONE;
-    private final static Block andesite = Blocks.ANDESITE;
-    private final static Block coalOre = Blocks.COAL_ORE;
-    private final static Block ironOre = Blocks.IRON_ORE;
-    private final static Block diamondOre = Blocks.DIAMOND_ORE;
-    private final static int startRadius = 4;
-
-    
-    public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> p_212245_2_, Random rand, BlockPos position, BlockBlobConfig p_212245_5_) {
-        
-        while (true)
-        {
-            label0:
-            { 
-        		if (position.getY() <= 6 || position.getY() >= 250)
-                {
-                    return false;
-                }
-            
-        		//this and position = position.down(); will keeps moving down position until it finds ground to generate on
-                if (world.isAirBlock(position.down()))
-                {
-                    break label0;
-                }
-                Block block = world.getBlockState(position.down()).getBlock();
+	private final static Block cobblestone = Blocks.COBBLESTONE;
+	private final static Block andesite = Blocks.ANDESITE;
+	private final static Block coalOre = Blocks.COAL_ORE;
+	private final static Block ironOre = Blocks.IRON_ORE;
+	private final static Block diamondOre = Blocks.DIAMOND_ORE;
+	private final static int startRadius = 4;
 
 
-            	//boulder will ignore other boulders and will generate only on ground
-            	if (block != Blocks.GRASS_BLOCK && !func_227250_b_(block))
-                {
-                    break label0;
-                }
+	public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> chunkgen, Random rand, BlockPos position, BlockBlobConfig config)
+	{
 
+		BlockState blockState = world.getBlockState(position);
 
-                for (int currentCount = 0; startRadius >= 0 && currentCount < 3; ++currentCount)
-                {
-                    int x = startRadius + rand.nextInt(2);
-                    int y = startRadius + rand.nextInt(2);
-                    int z = startRadius + rand.nextInt(2);
-                    float calculatedDistance = (float)(x + y + z) * 0.333F + 0.5F;
+		//Will keeps moving down position until it finds valid ground to generate on while ignoring other boulders
+		while (position.getY() >= 6)
+		{
+			if (blockState.getMaterial() == Material.AIR || 
+				(blockState.getBlock() != Blocks.GRASS_BLOCK && !func_227250_b_(blockState.getBlock())))
+			{
+				//block was air or a non-dirt/grass block. Thus move down one.
+				position = position.down();
+				blockState = world.getBlockState(position);
+			}
+			else
+			{
+				break; //We hit a valid spot to generate a boulder, time to exit loop
+			}
+		}
 
-                    for (BlockPos blockpos : BlockPos.getAllInBoxMutable(position.add(-x, -y, -z), position.add(x, y, z)))
-                    {
-                        if (blockpos.distanceSq(position) <= (double)(calculatedDistance * calculatedDistance))
-                        {
-                        	//adds the blocks for generation in this boulder
-                        	//note, if user turns off an ore, that ore's chance is dumped into the below ore for generation
-                        	int randomChance = rand.nextInt(1400);
-                        	
-                        	// 2/1400th chance for diamond ore
-                        	if(ConfigUA.diamondOreSpawnrate != 0 && randomChance <= 1) {
-                        		world.setBlockState(blockpos, diamondOre.getDefaultState(), 4);
-                        	}
-                        	
-                        	// 48/1400th chance for iron ore
-                        	else if(ConfigUA.ironOreSpawnrate != 0 && randomChance <= 50){
-                        		world.setBlockState(blockpos, ironOre.getDefaultState(), 4);
-                        	}
-                        	
-                        	// 82/1400th chance for coal ore
-                        	else if(ConfigUA.coalOreSpawnrate != 0 && randomChance <= 130){
-                        		world.setBlockState(blockpos, coalOre.getDefaultState(), 4);
-                        	}
-                        	
-                        	// 398/1400th chance for andesite
-                        	else if(randomChance <= 480){
-                        		world.setBlockState(blockpos, andesite.getDefaultState(), 4);
-                        	}
-                        	
-                        	// 352/1400th chance for cobblestone
-                        	else if(randomChance <= 750){
-                        		world.setBlockState(blockpos, cobblestone.getDefaultState(), 4);
-                        	}
-                        	
-                        	// 650/1400th chance for mossyCobblestone
-                        	else {
-                        		world.setBlockState(blockpos, mossyCobblestone.getDefaultState(), 4);
-                        	}
-                        }
-                    }
-                    position = position.add(-(startRadius + 1) + rand.nextInt(2 + startRadius * 2), 0 - rand.nextInt(2), -(startRadius + 1) + rand.nextInt(2 + startRadius * 2));
-               
-                    
-                }
-                //finished generating the boulder
-                return true;
-            }
-        
+		//if the height is too low or high, just quit.
+		if (position.getY() <= 6 || position.getY() >= 250)
+		{
+			return false;
+		}
 
-			//this and world.isAirBlock(position.down()) will keeps moving down position until it finds ground to generate on
-            position = position.down();
-        }
-    }
+		//we are at a valid spot to generate a boulder now. Begin generation.
+		for (int currentCount = 0; startRadius >= 0 && currentCount < 3; ++currentCount)
+		{
+			int x = startRadius + rand.nextInt(2);
+			int y = startRadius + rand.nextInt(2);
+			int z = startRadius + rand.nextInt(2);
+			float calculatedDistance = (float) (x + y + z) * 0.333F + 0.5F;
+
+			for (BlockPos blockpos : BlockPos.getAllInBoxMutable(position.add(-x, -y, -z), position.add(x, y, z)))
+			{
+				if (blockpos.distanceSq(position) <= (double) (calculatedDistance * calculatedDistance))
+				{
+					//adds the blocks for generation in this boulder
+					//note, if user turns off an ore, that ore's chance is dumped into the below ore for generation
+					int randomChance = rand.nextInt(1400);
+
+					// 2/1400th chance for diamond ore
+					if (ConfigUA.diamondOreSpawnrate != 0 && randomChance <= 1)
+					{
+						world.setBlockState(blockpos, diamondOre.getDefaultState(), 4);
+					}
+
+					// 48/1400th chance for iron ore
+					else if (ConfigUA.ironOreSpawnrate != 0 && randomChance <= 50)
+					{
+						world.setBlockState(blockpos, ironOre.getDefaultState(), 4);
+					}
+
+					// 82/1400th chance for coal ore
+					else if (ConfigUA.coalOreSpawnrate != 0 && randomChance <= 130)
+					{
+						world.setBlockState(blockpos, coalOre.getDefaultState(), 4);
+					}
+
+					// 398/1400th chance for andesite
+					else if (randomChance <= 480)
+					{
+						world.setBlockState(blockpos, andesite.getDefaultState(), 4);
+					}
+
+					// 352/1400th chance for cobblestone
+					else if (randomChance <= 750)
+					{
+						world.setBlockState(blockpos, cobblestone.getDefaultState(), 4);
+					}
+
+					// 650/1400th chance for mossyCobblestone
+					else
+					{
+						world.setBlockState(blockpos, mossyCobblestone.getDefaultState(), 4);
+					}
+				}
+			}
+			position = position.add(-(startRadius + 1) + rand.nextInt(2 + startRadius * 2), 0 - rand.nextInt(2), -(startRadius + 1) + rand.nextInt(2 + startRadius * 2));
+
+		}
+		//finished generating the boulder
+		return true;
+	}
 }
--- src/main/java/net/telepathicgrunt/ultraamplified/world/feature/BoulderNormal.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/world/feature/BoulderNormal.java
@@ -5,9 +5,9 @@ import java.util.function.Function;
 
 import com.mojang.datafixers.Dynamic;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -16,106 +16,108 @@ import net.minecraft.world.gen.feature.BlockBlobConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.telepathicgrunt.ultraamplified.config.ConfigUA;
 
-public class BoulderNormal extends Feature<BlockBlobConfig> 
+
+public class BoulderNormal extends Feature<BlockBlobConfig>
 {
-    public BoulderNormal(Function<Dynamic<?>, ? extends BlockBlobConfig> configFactoryIn) {
+	public BoulderNormal(Function<Dynamic<?>, ? extends BlockBlobConfig> configFactoryIn)
+	{
 		super(configFactoryIn);
 	}
 
-
 	private final static BlockState mossyCobblestone = Blocks.MOSSY_COBBLESTONE.getDefaultState();
-    private final static BlockState cobblestone = Blocks.COBBLESTONE.getDefaultState();
-    private final static BlockState andesite = Blocks.ANDESITE.getDefaultState();
-    private final static BlockState coalOre = Blocks.COAL_ORE.getDefaultState();
-    private final static BlockState ironOre = Blocks.IRON_ORE.getDefaultState();
-    private final static BlockState diamondOre = Blocks.DIAMOND_ORE.getDefaultState();
-    private final int startRadius = 3;
+	private final static BlockState cobblestone = Blocks.COBBLESTONE.getDefaultState();
+	private final static BlockState andesite = Blocks.ANDESITE.getDefaultState();
+	private final static BlockState coalOre = Blocks.COAL_ORE.getDefaultState();
+	private final static BlockState ironOre = Blocks.IRON_ORE.getDefaultState();
+	private final static BlockState diamondOre = Blocks.DIAMOND_ORE.getDefaultState();
+	private final int startRadius = 3;
 
 
-    public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> p_212245_2_, Random rand, BlockPos position, BlockBlobConfig p_212245_5_) {
-        
-        while (true)
-        {
-            label0:
-            { 
-        		if (position.getY() <= 6 || position.getY() >= 250)
-                {
-                    return false;
-                }
-            
-        		//this and position = position.down(); will keeps moving down position until it finds ground to generate on
-                if (world.isAirBlock(position.down()))
-                {
-                    break label0;
-                }
-                Block block = world.getBlockState(position.down()).getBlock();
+	public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> chunkgen, Random rand, BlockPos position, BlockBlobConfig config)
+	{
+		BlockState blockState = world.getBlockState(position);
 
-            
-            	//boulder will ignore other boulders and will generate only on ground
-            	if (block != Blocks.GRASS_BLOCK && !func_227250_b_(block))
-                {
-                    break label0;
-                }
+		//Will keeps moving down position until it finds valid ground to generate on while ignoring other boulders
+		while (position.getY() >= 6)
+		{
+			if (blockState.getMaterial() == Material.AIR || 
+				(blockState.getBlock() != Blocks.GRASS_BLOCK && !func_227250_b_(blockState.getBlock())))
+			{
+				//block was air or a non-dirt/grass block. Thus move down one.
+				position = position.down();
+				blockState = world.getBlockState(position);
+			}
+			else
+			{
+				break; //We hit a valid spot to generate a boulder, time to exit loop
+			}
+		}
 
-                int radius = startRadius;
+		//if the height is too low or high, just quit.
+		if (position.getY() <= 6 || position.getY() >= 250)
+		{
+			return false;
+		}
 
-                for (int currentCount = 0; radius >= 0 && currentCount < 3; ++currentCount)
-                {
-                    int x = radius + rand.nextInt(2);
-                    int y = radius + rand.nextInt(2);
-                    int z = radius + rand.nextInt(2);
-                    float calculatedDistance = (float)(x + y + z) * 0.333F + 0.5F;
+		//we are at a valid spot to generate a boulder now. Begin generation.
+		int radius = startRadius;
 
-                    for (BlockPos blockpos : BlockPos.getAllInBoxMutable(position.add(-x, -y, -z), position.add(x, y, z)))
-                    {
-                        if (blockpos.distanceSq(position) <= (double)(calculatedDistance * calculatedDistance))
-                        {
-                        	//adds the blocks for generation in this boulder
-                        	//note, if user turns off an ore, that ore's chance is dumped into the below ore for generation
-                        	int randomChance = rand.nextInt(1400);
-                        	
-                        	// 1/1400th chance for diamond ore
-                        	if(ConfigUA.diamondOreSpawnrate != 0 && randomChance == 0) {
-                        		world.setBlockState(blockpos, diamondOre, 4);
-                        	}
-                        	
-                        	// 39/1400th chance for iron ore
-                        	else if(ConfigUA.ironOreSpawnrate != 0 && randomChance <= 40){
-                        		world.setBlockState(blockpos, ironOre, 4);
-                        	}
-                        	
-                        	// 60/1400th chance for coal ore
-                        	else if(ConfigUA.coalOreSpawnrate != 0 && randomChance <= 100){
-                        		world.setBlockState(blockpos, coalOre, 4);
-                        	}
-                        	
-                        	// 300/1400th chance for andesite
-                        	else if(randomChance <= 400){
-                        		world.setBlockState(blockpos, andesite, 4);
-                        	}
-                        	
-                        	// 300/1400th chance for cobblestone
-                        	else if(randomChance <= 700){
-                        		world.setBlockState(blockpos, cobblestone, 4);
-                        	}
-                        	
-                        	// 700/1400th chance for mossyCobblestone
-                        	else {
-                        		world.setBlockState(blockpos, mossyCobblestone, 4);
-                        	}
-                        }
-                    }
-                    position = position.add(-(radius + 1) + rand.nextInt(2 + radius * 2), 0 - rand.nextInt(2), -(radius + 1) + rand.nextInt(2 + radius * 2));
-               
-                    
-                }
-                //finished generating the boulder
-                return true;
-            }
-        
+		for (int currentCount = 0; radius >= 0 && currentCount < 3; ++currentCount)
+		{
+			int x = radius + rand.nextInt(2);
+			int y = radius + rand.nextInt(2);
+			int z = radius + rand.nextInt(2);
+			float calculatedDistance = (float) (x + y + z) * 0.333F + 0.5F;
 
-			//this and world.isAirBlock(position.down()) will keeps moving down position until it finds ground to generate on
-            position = position.down();
-        }
-    }
+			for (BlockPos blockpos : BlockPos.getAllInBoxMutable(position.add(-x, -y, -z), position.add(x, y, z)))
+			{
+				if (blockpos.distanceSq(position) <= (double) (calculatedDistance * calculatedDistance))
+				{
+					//adds the blocks for generation in this boulder
+					//note, if user turns off an ore, that ore's chance is dumped into the below ore for generation
+					int randomChance = rand.nextInt(1400);
+
+					// 1/1400th chance for diamond ore
+					if (ConfigUA.diamondOreSpawnrate != 0 && randomChance == 0)
+					{
+						world.setBlockState(blockpos, diamondOre, 4);
+					}
+
+					// 39/1400th chance for iron ore
+					else if (ConfigUA.ironOreSpawnrate != 0 && randomChance <= 40)
+					{
+						world.setBlockState(blockpos, ironOre, 4);
+					}
+
+					// 60/1400th chance for coal ore
+					else if (ConfigUA.coalOreSpawnrate != 0 && randomChance <= 100)
+					{
+						world.setBlockState(blockpos, coalOre, 4);
+					}
+
+					// 300/1400th chance for andesite
+					else if (randomChance <= 400)
+					{
+						world.setBlockState(blockpos, andesite, 4);
+					}
+
+					// 300/1400th chance for cobblestone
+					else if (randomChance <= 700)
+					{
+						world.setBlockState(blockpos, cobblestone, 4);
+					}
+
+					// 700/1400th chance for mossyCobblestone
+					else
+					{
+						world.setBlockState(blockpos, mossyCobblestone, 4);
+					}
+				}
+			}
+			position = position.add(-(radius + 1) + rand.nextInt(2 + radius * 2), 0 - rand.nextInt(2), -(radius + 1) + rand.nextInt(2 + radius * 2));
+
+		}
+		//finished generating the boulder
+		return true;
+	}
 }
