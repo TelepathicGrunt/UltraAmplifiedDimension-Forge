e7a53f6bc782cb395a84415856bc00732222b56f
alex sarnese <alexthepokemon@Gmail.com>
2020-01-11 11:14:24 -0500

    optimized some code

--- README.txt
+++ README.txt
@@ -21,6 +21,7 @@ Just be sure to have fun and make awesome mods! That's all. Enjoy! :)
 ·Stone and Nether Fortresses now will naturally spawn enemies specific to their kind of fortress at a low rate. You can manually swap the kinds of bricks between the two fortresses to get the other fortress's natural spawning entities.
 ·Buffed loot from chest next to spawners in Stone and Nether Fortresses.    
 ·End Cities now naturally spawn Endermites at a low rate and very rarely, Phantoms too.
+·Optimized Glowpatches placement code a bit.
        
        
     (V.7.0.3 Changes) (1.15.1 Minecraft)
--- src/main/java/net/telepathicgrunt/ultraamplified/world/feature/placement/GlowstonePatchPlacement1.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/world/feature/placement/GlowstonePatchPlacement1.java
@@ -14,59 +14,67 @@ import net.minecraft.world.gen.placement.ChanceConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.telepathicgrunt.ultraamplified.config.ConfigUA;
 
-public class GlowstonePatchPlacement1 extends Placement<ChanceConfig> {
-   public GlowstonePatchPlacement1(Function<Dynamic<?>, ? extends ChanceConfig> configFactoryIn) {
+
+public class GlowstonePatchPlacement1 extends Placement<ChanceConfig>
+{
+	public GlowstonePatchPlacement1(Function<Dynamic<?>, ? extends ChanceConfig> configFactoryIn)
+	{
 		super(configFactoryIn);
 	}
 
-public Stream<BlockPos> getPositions(IWorld world, ChunkGenerator<? extends GenerationSettings> chunkGenerator, Random random, ChanceConfig chancesConfig, BlockPos pos) {
 
-	   int x = random.nextInt(16);
-	   int z = random.nextInt(16);
-	   int topYLayer = YPositionOfBelowLayer(world, 255, random, pos.add(x, 0 ,z));
-	   float chance = (ConfigUA.glowstoneVariantsSpawnrate/909f) * chancesConfig.chance;
-	   
-	   if(topYLayer < 75 || chance == 0) {
-		   return Stream.empty();
-	   }
-	   
-	   int height = random.nextInt(topYLayer-74) + 75;
-	   
-	     //gets y value of a layer below top layer
-	     topYLayer = YPositionOfBelowLayer(world, height, random, pos.add(x, 0 ,z));
-	     
-	     if(topYLayer < 75) {
-	    	 return Stream.empty();
-	     }
-	
-		 if (random.nextFloat() < chance) {
-			 return Stream.of(pos.add(x-4, topYLayer-1, z-4));
-		 }
+	public Stream<BlockPos> getPositions(IWorld world, ChunkGenerator<? extends GenerationSettings> chunkGenerator, Random random, ChanceConfig chancesConfig, BlockPos pos)
+	{
 
-		 return Stream.empty();
+		int x = random.nextInt(16);
+		int z = random.nextInt(16);
+		int topYLayer = YPositionOfBelowLayer(world, 255, random, pos.add(x, 0, z)); //Finds top layer
+		float chance = (ConfigUA.glowstoneVariantsSpawnrate / 909f) * chancesConfig.chance;
+
+		//Chance needs to be less than random number to generate patch
+		if (random.nextFloat() >= chance || topYLayer < 75 || chance == 0)
+		{
+			return Stream.empty();
+		}
+
+		//gets y value of a layer below top layer
+		int height = random.nextInt(topYLayer - 74) + 75;
+		topYLayer = YPositionOfBelowLayer(world, height, random, pos.add(x, 0, z));
+
+		if (topYLayer < 75) //make sure new height is not too low
+		{
+			return Stream.empty();
+		}
+
+		//offset so patch will be centered correctly when generating
+		return Stream.of(pos.add(x - 4, topYLayer - 1, z - 4));
 	}
-   
-	private int YPositionOfBelowLayer(IWorld world, int height, Random random, BlockPos pos) {
-		
-         
-         //if height is inside a non-air block, move down until we reached an air block
-         while(height > 74) {
-        	 if(world.isAirBlock(pos.add(0, height, 0))) {
-        		 break;
-        	 }
-        	 
-        	 height--;
-         }
-         
-         //if height is an air block, move down until we reached a solid block. We are now on the surface of a piece of land
-         while(height > 74) {
-        	 if(!world.isAirBlock(pos.add(0, height, 0))) {
-        		 break;
-        	 }
-        	 
-        	 height--;
-         }
-         
+
+
+	private int YPositionOfBelowLayer(IWorld world, int height, Random random, BlockPos pos)
+	{
+		//if height is inside a non-air block, move down until we reached an air block
+		while (height > 74)
+		{
+			if (world.isAirBlock(pos.add(0, height, 0)))
+			{
+				break;
+			}
+
+			height--;
+		}
+
+		//if height is an air block, move down until we reached a solid block. We are now on the surface of a piece of land
+		while (height > 74)
+		{
+			if (!world.isAirBlock(pos.add(0, height, 0)))
+			{
+				break;
+			}
+
+			height--;
+		}
+
 		return height;
 	}
 }
