87bce2603e58907d9bbce1224169e46f950a771d
alex sarnese <alexthepokemon@Gmail.com>
2020-02-12 10:33:03 -0500

    removed unneeded dimension fixing code

--- src/main/java/net/telepathicgrunt/ultraamplified/world/dimension/UltraAmplifiedDimension.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/world/dimension/UltraAmplifiedDimension.java
@@ -1,14 +1,7 @@
 package net.telepathicgrunt.ultraamplified.world.dimension;
 
-import java.lang.reflect.Field;
 import java.util.function.BiFunction;
-import java.util.function.IntSupplier;
 
-import org.apache.commons.lang3.tuple.Pair;
-
-import com.google.common.collect.ImmutableList;
-
-import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.Dimension;
@@ -19,14 +12,8 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.world.RegisterDimensionsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLHandshakeMessages;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import net.minecraftforge.fml.network.simple.SimpleChannel;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.telepathicgrunt.ultraamplified.UltraAmplified;
 
-
-@SuppressWarnings("deprecation")
 @Mod.EventBusSubscriber(modid = UltraAmplified.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class UltraAmplifiedDimension {
 
@@ -56,94 +43,8 @@ public class UltraAmplifiedDimension {
         RegUtil.generic(event.getRegistry()).add("ultraamplified", ULTRAAMPLIFIED);
     }
 
-    
-    //This forge fix is from this open source code: https://github.com/Cryptic-Mushroom/The-Midnight/blob/1.14.4-dev/src/main/java/com/mushroom/midnight/common/registry/MidnightDimensions.java
-    //Credit to Corail31 and Gegy
-    static {
-        // TODO: Temporary hack until Forge fix
-        try {
-            Field channelField = FMLNetworkConstants.class.getDeclaredField("handshakeChannel");
-            channelField.setAccessible(true);
-
-            SimpleChannel handshakeChannel = (SimpleChannel) channelField.get(null);
-            handshakeChannel.messageBuilder(S2CDimensionSync.class, 100)
-                    .loginIndex(S2CDimensionSync::getLoginIndex, S2CDimensionSync::setLoginIndex)
-                    .decoder(S2CDimensionSync::decode)
-                    .encoder(S2CDimensionSync::encode)
-                    .buildLoginPacketList(isLocal -> {
-                        if (isLocal) return ImmutableList.of();
-                        return ImmutableList.of(Pair.of("Ultra Amplified Dim Sync", new S2CDimensionSync(UltraAmplifiedDimension.ultraamplified())));
-                    })
-                    .consumer((msg, ctx) -> {
-                        if (DimensionManager.getRegistry().getByValue(msg.id) == null) {
-                            DimensionManager.registerDimensionInternal(msg.id, msg.name, msg.dimension, null, msg.skyLight);
-                        }
-                        ctx.get().setPacketHandled(true);
-                        handshakeChannel.reply(new FMLHandshakeMessages.C2SAcknowledge(), ctx.get());
-                    })
-                    .add();
-        } catch (ReflectiveOperationException e) {
-            UltraAmplified.LOGGER.error("Failed to add dimension sync to handshake channel", e);
-        }
-    }
 	
-
     public static DimensionType ultraamplified() {
         return DimensionType.byName(ULTRAAMPLIFIED_ID);
-    }
-    
-    
-    //This forge fix is from this open source code: https://github.com/Cryptic-Mushroom/The-Midnight/blob/1.14.4-dev/src/main/java/com/mushroom/midnight/common/registry/MidnightDimensions.java
-    //Credit to Corail31 and Gegy
-    public static class S2CDimensionSync implements IntSupplier{
-        final int id;
-        final ResourceLocation name;
-        final ModDimension dimension;
-        final boolean skyLight;
-
-        private int loginIndex;
-
-        public S2CDimensionSync(DimensionType dimensionType) {
-            this.id = dimensionType.getId() + 1;
-            this.name = DimensionType.getKey(dimensionType);
-            this.dimension = dimensionType.getModType();
-            this.skyLight = dimensionType.hasSkyLight();
-        }
-
-        S2CDimensionSync(int id, ResourceLocation name, ModDimension dimension, boolean skyLight) {
-            this.id = id;
-            this.name = name;
-            this.dimension = dimension;
-            this.skyLight = skyLight;
-        }
-
-        void setLoginIndex(final int loginIndex) {
-            this.loginIndex = loginIndex;
-        }
-
-        int getLoginIndex() {
-            return this.loginIndex;
-        }
-
-        void encode(PacketBuffer buffer) {
-            buffer.writeInt(this.id);
-            buffer.writeResourceLocation(this.name);
-            buffer.writeResourceLocation(this.dimension.getRegistryName());
-            buffer.writeBoolean(this.skyLight);
-        }
-
-        public static S2CDimensionSync decode(PacketBuffer buffer) {
-            int id = buffer.readInt();
-            ResourceLocation name = buffer.readResourceLocation();
-            ModDimension dimension = ForgeRegistries.MOD_DIMENSIONS.getValue(buffer.readResourceLocation());
-            boolean skyLight = buffer.readBoolean();
-
-            return new S2CDimensionSync(id, name, dimension, skyLight);
-        }
-
-		@Override
-		public int getAsInt() {
-	         return this.getLoginIndex();
-		}
     }
 }
