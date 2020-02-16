24cb5f16447009edba53b346b25ea44172c42a5f
alex sarnese <alexthepokemon@Gmail.com>
2020-02-16 15:14:57 -0500

    optimized containsundergroundliquids

--- src/main/java/net/telepathicgrunt/ultraamplified/world/feature/ContainUndergroundLiquids.java
+++ src/main/java/net/telepathicgrunt/ultraamplified/world/feature/ContainUndergroundLiquids.java
@@ -9,6 +9,7 @@ import com.mojang.datafixers.Dynamic;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
@@ -19,8 +20,11 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.telepathicgrunt.ultraamplified.world.biome.BiomeInit;
 
-public class ContainUndergroundLiquids extends Feature<NoFeatureConfig> {
-	public ContainUndergroundLiquids(Function<Dynamic<?>, ? extends NoFeatureConfig> configFactory) {
+
+public class ContainUndergroundLiquids extends Feature<NoFeatureConfig>
+{
+	public ContainUndergroundLiquids(Function<Dynamic<?>, ? extends NoFeatureConfig> configFactory)
+	{
 		super(configFactory);
 	}
 
@@ -30,11 +34,14 @@ public class ContainUndergroundLiquids extends Feature<NoFeatureConfig> {
 
 	private static Map<Biome, BlockState> fillerBiomeMap;
 
+
 	/**
 	 * Have to make this map in UltraAmplified setup method since the biomes needs to be initialized first
 	 */
-	public static void setFillerMap() {
-		if (fillerBiomeMap == null) {
+	public static void setFillerMap()
+	{
+		if (fillerBiomeMap == null)
+		{
 			fillerBiomeMap = new HashMap<Biome, BlockState>();
 
 			fillerBiomeMap.put(BiomeInit.NETHERLAND, Blocks.NETHERRACK.getDefaultState());
@@ -47,63 +54,90 @@ public class ContainUndergroundLiquids extends Feature<NoFeatureConfig> {
 		}
 	}
 
-	public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> chunkSettings, Random random, BlockPos position, NoFeatureConfig configBlock) {
-		
+
+	public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> chunkSettings, Random random, BlockPos position, NoFeatureConfig configBlock)
+	{
+
 		boolean notContainedFlag;
 		BlockState currentblock;
 		BlockPos.Mutable blockpos$Mutable = new BlockPos.Mutable(position.getX(), 0, position.getZ());  //set y to 0
 
-		for (int x = 0; x < 16; ++x) {
-			for (int z = 0; z < 16; ++z) {
+		for (int x = 0; x < 16; ++x)
+		{
+			for (int z = 0; z < 16; ++z)
+			{
 
-				for (int y = 61; y >= 10; y--) {
+				/*
+				 * for (int y = 61; y >= 10; y--) {
+				 * 
+				 * blockpos$Mutable.setPos(position.getX() + x, y, position.getZ() + z); 
+				 * currentblock = world.getBlockState(blockpos$Mutable);
+				 * 
+				 * // move down until we hit an air block 
+				 * while (currentblock != Blocks.AIR.getDefaultState() && blockpos$Mutable.getY() > 11) 
+				 * { 
+				 * 		blockpos$Mutable.move(Direction.DOWN); 
+				 * 		currentblock = world.getBlockState(blockpos$Mutable); 
+				 * }
+				 * 
+				 * //checks one last time at y = 11 to see if we should quit now 
+				 * if(blockpos$Mutable.getY() <= 11 && currentblock != Blocks.AIR.getDefaultState()) 
+				 * { 
+				 * 		continue; 
+				 * }
+				 */
 
-					blockpos$Mutable.setPos(position.getX() + x, y, position.getZ() + z);
+				blockpos$Mutable.setPos(position.getX() + x, 61, position.getZ() + z);
+				for (; blockpos$Mutable.getY() > 10; blockpos$Mutable.move(Direction.DOWN))
+				{
+
 					currentblock = world.getBlockState(blockpos$Mutable);
 
 					// move down until we hit an air block
-					while (currentblock != Blocks.AIR.getDefaultState() && blockpos$Mutable.getY() > 11) {
-						blockpos$Mutable.move(Direction.DOWN);
-						currentblock = world.getBlockState(blockpos$Mutable);
+					while (currentblock.getMaterial() != Material.AIR && blockpos$Mutable.getY() > 10)
+					{
+						currentblock = world.getBlockState(blockpos$Mutable.move(Direction.DOWN));
 					}
-					
-					//checks one last time at y = 11 to see if we should quit now
-					if(blockpos$Mutable.getY() <= 11 && currentblock != Blocks.AIR.getDefaultState()) {
-						continue;
+
+					//if too low, break and go to next xz coordinate
+					if (blockpos$Mutable.getY() <= 10)
+					{
+						break;
 					}
-					
 
 					// y value is now fully set for rest of code
 					// checks to see if we are touching a liquid block
 					notContainedFlag = false;
 
-					for (Direction face : Direction.Plane.HORIZONTAL) {
+					for (Direction face : Direction.Plane.HORIZONTAL)
+					{
 
 						currentblock = world.getBlockState(blockpos$Mutable.offset(face));
-						if (!currentblock.getFluidState().isEmpty()) {
+						if (!currentblock.getFluidState().isEmpty())
+						{
 							notContainedFlag = true;
 						}
 					}
 
-					blockpos$Mutable.move(Direction.UP);
-					currentblock = world.getBlockState(blockpos$Mutable);
-					if (!currentblock.getFluidState().isEmpty()) {
+					currentblock = world.getBlockState(blockpos$Mutable.up());
+					if (!currentblock.getFluidState().isEmpty())
+					{
 						notContainedFlag = true;
 					}
 
-					blockpos$Mutable.move(Direction.DOWN, 2); //move from above block to below block 
-					currentblock = world.getBlockState(blockpos$Mutable);
-					if (blockpos$Mutable.getY() > 10 && !currentblock.getFluidState().isEmpty()) {
+					currentblock = world.getBlockState(blockpos$Mutable.down());
+					if (blockpos$Mutable.down().getY() > 10 && !currentblock.getFluidState().isEmpty())
+					{
 						notContainedFlag = true;
 					}
 
 					// this air block is touching liquid, time to make it solid
-					if (notContainedFlag) {
-
-						blockpos$Mutable.move(Direction.UP); //move back to center block
+					if (notContainedFlag)
+					{
 						//grabs what block to use based on what biome we are in
 						replacementBlock = fillerBiomeMap.get(world.getBiome(blockpos$Mutable));
-						if (replacementBlock == null) {
+						if (replacementBlock == null)
+						{
 							replacementBlock = STONE;
 						}
 
